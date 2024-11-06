523be314c3b4e83ff8c988835bc30a2e4696346c
jpmoyses <jpmoyses00@gmail.com>
2024-11-06 16:39:58 -0300

    Completed the shopping list site

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ShoppingList
+This project was made with the intent of learning the basics of DOM manipulation, event handlers and overall structure of javascript in HTML.
---
+++ javascript.js
@@ -0,0 +1,36 @@
+const btn = document.getElementById("mainBtn");
+const input = document.getElementById("item");
+const list = document.getElementById("list");
+let value;
+
+// capitalizes only the first letter of the phrase
+function capitalizePhrase(value){
+    value = value.toLowerCase();
+    return String(value).charAt(0).toUpperCase() + String(value).slice(1);
+}
+
+btn.addEventListener("click", () => {
+    // creating the elements
+    const listItem = document.createElement("li");
+    const span = document.createElement("span");
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.classList.add("btn", "altBtn");
+
+    // adding value to elements and adding children
+    value = input.value;
+    value = capitalizePhrase(value);
+    input.value = '';
+    span.innerText = value;
+    listItem.appendChild(span);
+    listItem.appendChild(deleteButton);
+    list.appendChild(listItem);
+    
+    // delete button
+    deleteButton.addEventListener("click", () =>{
+        listItem.remove();
+    });
+
+    // returns the focus to the input box
+    input.focus();
+})
